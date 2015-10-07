5c5477dd3ab5480c862acf5365c5b068cf465ea3
xtrot <enddyygf93@live.com>
2015-10-07 10:52:18 -0400

    Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello World' });
 });
 
+router.get('/boot/', function(req, res, next) {
+  res.render('boot/index', { title: 'Bootstraping!' });
+});
+
 module.exports = router;
